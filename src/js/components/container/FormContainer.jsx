49440e55e207d01e6921a09fa7ce2d007d92a625
usbpendrive <ferry.fernando@gmail.com>
2019-01-06 15:09:29 +0700

    React dependencies.

---
+++ src/js/components/container/FormContainer.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Input from "../presentational/Input.jsx";
+
+class FormContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            seo_title: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.id]: event.target.value });
+    }
+
+    render() {
+        const { seo_title } = this.state;
+        return (
+            <form id="article-form">
+                <Input label="seo_title" text="SEO title" type="text"
+                       id="seo_title" value={seo_title} handleChange={this.handleChange}
+                />
+            </form>
+        )
+    }
+}
+
+export default FormContainer;
